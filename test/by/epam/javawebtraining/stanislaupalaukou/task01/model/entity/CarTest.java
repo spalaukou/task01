7cabd735ff584c4a5dbb9eec34d4e0c7996ed987
spalaukou <Ksenia9091Stas>
2019-03-13 18:15:19 +0300

    CarTest has been updated

--- test/by/epam/javawebtraining/stanislaupalaukou/task01/model/entity/CarTest.java
+++ test/by/epam/javawebtraining/stanislaupalaukou/task01/model/entity/CarTest.java
@@ -1,9 +1,6 @@
 package by.epam.javawebtraining.stanislaupalaukou.task01.model.entity;
 
-import by.epam.javawebtraining.stanislaupalaukou.task01.model.exception.logical.CarBodyTypeException;
-import by.epam.javawebtraining.stanislaupalaukou.task01.model.exception.logical.SeatsNumberException;
-import by.epam.javawebtraining.stanislaupalaukou.task01.model.exception.logical.VehicleNameException;
-import by.epam.javawebtraining.stanislaupalaukou.task01.model.exception.logical.VehiclePriceException;
+import by.epam.javawebtraining.stanislaupalaukou.task01.model.exception.logical.*;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -20,6 +17,21 @@ public class CarTest {
     public void setUpCar()
             throws SeatsNumberException, VehiclePriceException, VehicleNameException, CarBodyTypeException {
         car = new Car ("BMW", 66_000, 4, Car.BodyType.SEDAN);
+    }
+
+    @Test
+    public void testDefaultConstructorCar() {
+        Car actual = new Car();
+
+        String expectedName = null;
+        int expectedPrice = 0;
+        int expectedSeatsNumber = 0;
+        Car.BodyType expectedBodyType = null;
+
+        Assert.assertEquals(expectedName, actual.getName());
+        Assert.assertEquals(expectedPrice, actual.getPrice());
+        Assert.assertEquals(expectedSeatsNumber, actual.getSeatsNumber());
+        Assert.assertEquals(expectedBodyType, actual.getBodyType());
     }
 
     @Test
@@ -40,6 +52,19 @@ public class CarTest {
         Assert.assertEquals(expectedBodyType, actualBodyType);
     }
 
+    @Test (expectedExceptions = CarBodyTypeException.class)
+    public void testConstructorCarException()
+            throws SeatsNumberException, VehiclePriceException, VehicleNameException, CarBodyTypeException {
+        new Car ("BMW", 66_000, 4, null);
+    }
+
+    @Test
+    public void testCopyConstructorCar() throws SeatsNumberException, VehiclePriceException, VehicleNameException, CarBodyTypeException {
+        Car expectedCar = new Car(car);
+
+        Assert.assertEquals(expectedCar, car);
+    }
+
     @Test
     public void testSetBodyType() throws CarBodyTypeException {
         Car.BodyType expectedBodyType = Car.BodyType.HATCHBACK;
@@ -49,4 +74,11 @@ public class CarTest {
 
         Assert.assertEquals(expectedBodyType, actualBodyType);
     }
+
+    @Test (expectedExceptions = CarBodyTypeException.class)
+    public void testSetBodyTypeException() throws CarBodyTypeException {
+        car.setBodyType(null);
+    }
+
+
 }
